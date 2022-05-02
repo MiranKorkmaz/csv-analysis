a11dda75e61a4fbbda29aba529a67a0d793d5d2a
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-05-02 21:53:41 +0200

    putting it all together

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from "./MatchReader"
 import { CSVFileReader } from "./CSVFileReader"
-import { MatchResult } from "./MatchResult"
+import { ConsoleReport } from "./reportTargets/ConsoleReport"
+import { WinsAnalysis } from "./analyzers/WinsAnalysis"
+import { Summary } from "./Summary"
 
 // Create an object that satifies the "DataReader" interface
 const csvFileReader = new CSVFileReader("football.csv")
@@ -10,4 +12,10 @@ const csvFileReader = new CSVFileReader("football.csv")
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
+const summary = new Summary(
+    new WinsAnalysis("Man United"),
+    new ConsoleReport()
+)
 
+
+summary.buildAndPrintReport(matchReader.matches)
